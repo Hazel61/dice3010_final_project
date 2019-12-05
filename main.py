b4dee666dd5bb0046345287ac229ce91af87ac4d
Shannon Michaels <michaelsshan@seattleu.edu>
2019-12-04 21:36:38 -0800

    Working

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import math
 import random
 import turtle
+import os.path
 
-# This is a turtle race game inspired by the Raspberry Pi Foundation
+# This is a turtle race game inspired by the Raspberry Pi Foundation. Set the screen size and the number of turtles to
+# race.
 win_length = 500
 win_height = 500
 turtles = 7
 turtle.screensize(win_length, win_height)
+
+# define a turtle racer object. Each turtle will have properties that show its color, shape, position, direction,
+# and score. Each turtle will have a move function and a reset function.
 
 
 class Racer(object):
@@ -32,16 +37,41 @@ class Racer(object):
         self.player.setpos(self.pos)
 
 
+def setup_file(name, colors):
+    file = open(name, 'w')
+    for color in colors:
+        file.write(color + ' 0 \n')
+    file.close()
+
+
+def update_scores(racers):
+    with open('scores.txt', 'w') as f:
+        for racer in racers:
+            f.write(str(racer.color) + ' ' + str(racer.score) + '\n')
+
+
 def start_game():
     racer_list = []
     turtle.clearscreen()
     turtle.hideturtle()
+    start_pos = -(win_length / 2) + 20
     colors = ["red", "green", "blue", 'yellow', 'magenta', 'orange', 'purple']
-    start_pos = -(win_length/2) + 20
+    # Iterate through each of the 7 turtles.
     for t in range(turtles):
-        new_pos_x = start_pos + t * win_length//turtles
-        racer_list.append(Racer(colors[t],(new_pos_x, -230)))
+        new_pos_x = start_pos + t * win_length // turtles
+        racer_list.append(Racer(colors[t], (new_pos_x, -230)))
         racer_list[t].player.showturtle()
+
+    if not os.path.exists('scores.txt'):
+        setup_file("scores.txt", colors)
+    else:
+        # read the file for scores
+        file = open('scores.txt', 'r')
+        for line in file:
+            color, score = line.split()
+            racer = next(racer for racer in racer_list if racer.color == color)
+            racer.score = int(score)
+        file.close()
 
     run = True
     while run:
@@ -63,37 +93,7 @@ def start_game():
             run = False
             print('The winner is: ' + ', '.join([winner.color for winner in winners]))
 
-    # oldScore = []
-    # file = open('scores.txt', 'r')
-    # for line in file:
-    #     l = line.split()
-    #     color = l[0]
-    #     score = l[1]
-    #     oldScore.append([color, score])
-    #
-    # file.close()
-    #
-    # file = open('scores.txt', 'w')
-    #
-    # for entry in oldScore:
-    #     for winner in max_color:
-    #         if entry[0] == winner:
-    #             entry[1] = int(entry[1]) + 1
-    #
-    #     file.write(str(entry[0]) + ' ' + str(entry[1]) + '\n')
-    #
-    #
-    # file.close()
-
-
-def setup_file(name, colors):
-    file = open(name, 'w')
-    for color in colors:
-        file.write(color + ' 0 \n')
-    file.close()
-
-
-setup_file("scores.txt", ["red", "green", "blue", 'yellow', "magenta", "orange", "purple"])
+    update_scores(racer_list)
 
 
 start = input('Would you like to play? ')
